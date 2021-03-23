073511df234b9297ff220b91bc1ce31e66a55d71
NGUYỄN THÀNH VĨNH PHÚC <1712675@student.hcmus.edu.vn>
2021-03-23 20:37:52 +0700

    to checkout phuc

--- lib/screens/chat/components/chat_card_choose_new.dart
+++ lib/screens/chat/components/chat_card_choose_new.dart
@@ -5,55 +5,53 @@ import '../../../configs/constants/color.dart';
 import '../../../models/chat/chat.dart';
 
 class ChatCardAddNew extends StatefulWidget {
-  ChatCardAddNew({
+  const ChatCardAddNew({
     Key? key,
-    required this.chat,
-    required this.press,
-    required this.isSelected,
   }) : super(key: key);
-
-  final Chat chat;
-  bool isSelected;
-  final VoidCallback press;
 
   @override
   _ChatCardAddNewState createState() => _ChatCardAddNewState();
-  @override
-  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
-    super.debugFillProperties(properties);
-    properties.add(DiagnosticsProperty<Chat>('chat', chat));
-    properties.add(DiagnosticsProperty<bool>('isSelected', isSelected));
-    properties.add(ObjectFlagProperty<VoidCallback>.has('press', press));
-  }
 }
 
 class _ChatCardAddNewState extends State<ChatCardAddNew> {
+  late Chat chat;
+  late bool isSelected;
+  late VoidCallback press;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 10),
       child: CheckboxListTile(
         title: Text(
-          widget.chat.name,
+          chat.name,
           style: const TextStyle(
             fontSize: 16,
             fontWeight: FontWeight.w500,
           ),
         ),
-        value: widget.isSelected,
+        value: isSelected,
         activeColor: kPrimaryColor,
         checkColor: Colors.white,
         onChanged: (value) {
           setState(() {
-            widget.isSelected = value!;
+            isSelected = value!;
           });
         },
         secondary: CircleAvatar(
           radius: 24,
-          backgroundImage: AssetImage(widget.chat.image),
+          backgroundImage: AssetImage(chat.image),
         ),
         controlAffinity: ListTileControlAffinity.trailing,
       ),
     );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty<Chat>('chat', chat));
+    properties.add(DiagnosticsProperty<bool>('isSelected', isSelected));
+    properties.add(ObjectFlagProperty<VoidCallback>.has('press', press));
   }
 }
