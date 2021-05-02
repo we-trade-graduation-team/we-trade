781eff1e6ba20090ef4880d953fa1f017b0811e9
Duy Quang <61661668+duyquang19981@users.noreply.github.com>
2021-05-02 14:53:58 +0700

    cloud_firestore config for my temp database

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
 import 'app.dart';
 
-
-void main() => runApp( const MyApp());
+// ignore: non_constant_identifier_names
+bool USE_FIRESTORE_EMULATOR = false;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  if (USE_FIRESTORE_EMULATOR) {
+    FirebaseFirestore.instance.settings = const Settings(
+        host: 'localhost:8080', sslEnabled: false, persistenceEnabled: false);
+  }
+  runApp(const MyApp());
+}
