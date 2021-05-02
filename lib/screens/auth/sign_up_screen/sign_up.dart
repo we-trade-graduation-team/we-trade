33fb2ad48262c8f156cc576bd4f1421faf5ca4ce
NGUYỄN THÀNH VĨNH PHÚC <1712675@student.hcmus.edu.vn>
2021-05-02 20:48:25 +0700

    update sign up screen

--- lib/screens/auth/sign_up_screen/sign_up.dart
+++ lib/screens/auth/sign_up_screen/sign_up.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'local_widgets/body.dart';
 
@@ -11,8 +12,14 @@ class SignUpScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarIconBrightness: Brightness.light,
+      ),
+    );
     return const Scaffold(
       body: Body(),
+      backgroundColor: Colors.black,
       resizeToAvoidBottomInset: false,
     );
   }
