a7359fdb465e10f40ff1a011c4ce1bcd75ff1eab
NGUYỄN THÀNH VĨNH PHÚC <1712675@student.hcmus.edu.vn>
2021-05-02 20:50:39 +0700

    update home screen

--- lib/screens/home_screen/local_widgets/icon_button_with_counter.dart
+++ lib/screens/home_screen/local_widgets/icon_button_with_counter.dart
@@ -2,53 +2,48 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 // import 'package:flutter_svg/flutter_svg.dart';
 import 'package:line_icons/line_icons.dart';
-import '../../../configs/constants/color.dart';
 
-class IconBtnWithCounter extends StatelessWidget {
-  const IconBtnWithCounter({
+class IconButtonWithCounter extends StatelessWidget {
+  const IconButtonWithCounter({
     Key? key,
     required this.icon,
-    this.numOfitem = 0,
+    this.numOfItems = 0,
     required this.press,
   }) : super(key: key);
 
   final String icon;
-  final int numOfitem;
+  final int numOfItems;
   final GestureTapCallback press;
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
+    // final size = MediaQuery.of(context).size;
     return InkWell(
       borderRadius: BorderRadius.circular(100),
       onTap: press,
       child: Stack(
         clipBehavior: Clip.none,
         children: [
-          Container(
-            // padding: EdgeInsets.all(size.width * 0.025),
-            height: size.height * 0.06,
-            width: size.width * 0.12,
-            decoration: BoxDecoration(
-              color: kSecondaryColor.withOpacity(0.1),
-              shape: BoxShape.circle,
-            ),
-            // child: SvgPicture.asset(
-            //   icon,
-            //   color: kPrimaryColor.withOpacity(0.8),
-            // ),
-            child: Icon(
-              LineIcons.values[icon],
-              color: kPrimaryColor.withOpacity(0.8),
+          AspectRatio(
+            aspectRatio: 1,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.transparent,
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                LineIcons.values[icon],
+                color: Colors.white,
+              ),
             ),
           ),
-          if (numOfitem != 0)
+          if (numOfItems != 0)
             Positioned(
-              top: -3,
-              right: 0,
+              top: 5,
+              right: 5,
               child: Container(
-                height: size.width * 0.04,
-                width: size.width * 0.04,
+                height: 16,
+                width: 16,
                 decoration: BoxDecoration(
                   color: const Color(0xFFFF4848),
                   shape: BoxShape.circle,
@@ -59,10 +54,10 @@ class IconBtnWithCounter extends StatelessWidget {
                 ),
                 child: Center(
                   child: Text(
-                    '$numOfitem',
+                    '$numOfItems',
                     style: const TextStyle(
                       fontSize: 8,
-                      height: 1,
+                      height: 1.2,
                       fontWeight: FontWeight.w600,
                       color: Colors.white,
                     ),
@@ -79,7 +74,7 @@ class IconBtnWithCounter extends StatelessWidget {
   void debugFillProperties(DiagnosticPropertiesBuilder properties) {
     super.debugFillProperties(properties);
     properties.add(StringProperty('icon', icon));
-    properties.add(IntProperty('numOfitem', numOfitem));
+    properties.add(IntProperty('numOfItems', numOfItems));
     properties.add(ObjectFlagProperty<GestureTapCallback>.has('press', press));
   }
 }
