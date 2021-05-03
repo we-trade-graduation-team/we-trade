6517849b7bd019be3953e204acce08007911de84
NGUYỄN THÀNH VĨNH PHÚC <1712675@student.hcmus.edu.vn>
2021-05-03 15:57:39 +0700

    remove debug banner

--- lib/app.dart
+++ lib/app.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
     // );
 
     return MaterialApp(
-      // debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       builder: BotToastInit(),
       navigatorObservers: [BotToastNavigatorObserver()],
       theme: themeData(),
