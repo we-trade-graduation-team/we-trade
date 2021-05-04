25590e4b191a39d6ae74c96c8ccc4818cacb9595
VÕ THIỆN TÍN <vothientin@gmail.com>
2021-05-04 23:33:06 +0700

    Update posting_features_routes.dart

--- lib/routing/posting_features_routes.dart
+++ lib/routing/posting_features_routes.dart
@@ -5,7 +5,7 @@ import '../screens/post_items/step3.dart';
 import '../screens/post_items/step4.dart';
 
 // ignore: non_constant_identifier_names
-final Map<String, WidgetBuilder> routes_postitem = {
+final Map<String, WidgetBuilder> postingFeaturesRoutes = {
   //PostItems1.routeName: (context) => const PostItems1(),
   PostItem_2.routeName: (context) => const PostItem_2(),
   PostItem_3.routeName: (context) => const PostItem_3(),
