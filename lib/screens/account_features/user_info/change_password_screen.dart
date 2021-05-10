15f414ecfc05ebd43e54eb4a611c68163f8735da
Duy Quang <61661668+duyquang19981@users.noreply.github.com>
2021-05-10 16:36:43 +0700

    validate in change password screen

--- lib/screens/account_features/user_info/change_password_screen.dart
+++ lib/screens/account_features/user_info/change_password_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import '../../../configs/constants/color.dart';
 
 class ChangePasswordScreen extends StatefulWidget {
@@ -17,8 +19,45 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
   final _newPasswordController = TextEditingController();
   final _confirmPasswordController = TextEditingController();
 
+  final _formKey = GlobalKey<FormBuilderState>();
+  late FocusScopeNode node;
+
+  void _onEdittingCompleteHandleFunction() {
+    node.unfocus();
+  }
+
+  Future<void> _showMyDialog(
+      String title, String content, Function handleFunction) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (context) {
+        return AlertDialog(
+          title: Text(title),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: [
+                Text(content),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                handleFunction();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    node = FocusScope.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Đổi mật khẩu'),
@@ -33,33 +72,63 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
             vertical: 30,
           ),
           children: [
-            Column(
-              children: [
-                TextField(
-                  decoration: const InputDecoration(
-                    labelText: 'Mật khẩu cũ',
+            FormBuilder(
+              key: _formKey,
+              onChanged: () {},
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: Column(
+                children: [
+                  FormBuilderTextField(
+                    name: 'oldpassword',
+                    decoration: const InputDecoration(
+                      labelText: 'Mật khẩu cũ',
+                      floatingLabelBehavior: FloatingLabelBehavior.auto,
+                    ),
+                    validator: FormBuilderValidators.compose([
+                      FormBuilderValidators.required(context),
+                    ]),
+                    obscureText: true,
+                    onEditingComplete: _onEdittingCompleteHandleFunction,
+                    textInputAction: TextInputAction.done,
+                    keyboardType: TextInputType.text,
+                    controller: _oldPasswordController,
                   ),
-                  obscureText: true,
-                  controller: _oldPasswordController,
-                ),
-                const SizedBox(height: 15),
-                TextField(
-                  decoration: const InputDecoration(
-                    labelText: 'Mật khẩu mới',
+                  const SizedBox(height: 15),
+                  FormBuilderTextField(
+                    name: 'newpassword',
+                    decoration: const InputDecoration(
+                      labelText: 'Mật khẩu mới',
+                      floatingLabelBehavior: FloatingLabelBehavior.auto,
+                    ),
+                    validator: FormBuilderValidators.compose([
+                      FormBuilderValidators.required(context),
+                      FormBuilderValidators.minLength(context, 6),
+                    ]),
+                    obscureText: true,
+                    onEditingComplete: _onEdittingCompleteHandleFunction,
+                    textInputAction: TextInputAction.done,
+                    keyboardType: TextInputType.text,
+                    controller: _newPasswordController,
                   ),
-                  obscureText: true,
-                  controller: _newPasswordController,
-                ),
-                const SizedBox(height: 15),
-                TextField(
-                  decoration: const InputDecoration(
-                    labelText: 'Xác nhận mật khẩu mới',
+                  const SizedBox(height: 15),
+                  FormBuilderTextField(
+                    name: 'confirmpassword',
+                    decoration: const InputDecoration(
+                      labelText: 'Xác nhận mật khẩu mới',
+                      floatingLabelBehavior: FloatingLabelBehavior.auto,
+                    ),
+                    validator: FormBuilderValidators.compose([
+                      FormBuilderValidators.required(context),
+                      FormBuilderValidators.minLength(context, 6),
+                    ]),
+                    obscureText: true,
+                    onEditingComplete: _onEdittingCompleteHandleFunction,
+                    textInputAction: TextInputAction.done,
+                    keyboardType: TextInputType.text,
+                    controller: _confirmPasswordController,
                   ),
-                  obscureText: true,
-                  controller: _confirmPasswordController,
-                ),
-                const SizedBox(height: 15),
-              ],
+                ],
+              ),
             ),
           ],
         ),
@@ -71,7 +140,26 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
           children: [
             Expanded(
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  _formKey.currentState!.save();
+                  if (_formKey.currentState!.validate()) {
+                    if (_confirmPasswordController.text !=
+                        _newPasswordController.text) {
+                      _showMyDialog(
+                          'Thông báo', 'Xác nhận mật khẩu không chính xác.',
+                          () {
+                        Navigator.of(context).pop();
+                      });
+                    } else {
+                      //TODO: compare pw in db
+                      _showMyDialog(
+                          'Thông báo', 'Thay đổi mật khẩu thành công.', () {
+                        Navigator.of(context).pop();
+                        Navigator.of(context).pop();
+                      });
+                    }
+                  }
+                },
                 style: ElevatedButton.styleFrom(
                   primary: kPrimaryColor,
                 ),
@@ -82,5 +170,11 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
         ),
       ),
     );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty<FocusScopeNode>('node', node));
   }
 }
