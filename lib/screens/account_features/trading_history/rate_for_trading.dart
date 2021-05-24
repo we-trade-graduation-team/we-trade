2d604a0fedf83630c14d249516460940faa67ad7
Duy Quang <61661668+duyquang19981@users.noreply.github.com>
2021-05-25 03:25:31 +0700

    shown data on rate_for_trading screen

--- lib/screens/account_features/trading_history/rate_for_trading.dart
+++ lib/screens/account_features/trading_history/rate_for_trading.dart
@@ -1,7 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
 import '../../../configs/constants/assets_paths/shared_assets_root.dart';
 import '../../../configs/constants/color.dart';
 import '../../../models/product/temp_class.dart';
+import '../account/account_screen.dart';
+import '../account/local_widgets/getter.dart';
 import '../shared_widgets/rating_bar.dart';
 
 class RateForTrading extends StatefulWidget {
@@ -16,51 +21,19 @@ class RateForTrading extends StatefulWidget {
 }
 
 class _RateForTradingState extends State<RateForTrading> {
-  // late int _rating;
-
-  List<Widget> _buildProductsList(List<Product> products) {
-    final widget = <Widget>[];
-    for (var i = 0; i < products.length; i++) {
-      widget.add(
-        Row(
-          children: [
-            Container(
-              width: 70,
-              height: 70,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.asset(
-                  products[i].images[0],
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            const SizedBox(width: 30),
-            Expanded(
-              child: Text(
-                products[i].name,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 2,
-                style: const TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
-      widget.add(const SizedBox(height: 10));
-    }
-
-    return widget;
-  }
+  final userID = AccountScreen.localUserID;
+  final referenceDatabase = AccountScreen.localRefDatabase;
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final args =
+        ModalRoute.of(context)!.settings.arguments as Map<String, String>;
+    final otherSideUserID = args['otherSideUserID'].toString();
+    final tradingID = args['tradingID'].toString();
     final products = [
-      // {'name': 'Product 1', 'img': ''},
-      // {'name': 'Product 2', 'img': ''},
-      productsData[0], productsData[1],
+      productsData[0],
+      productsData[1],
     ];
 
     const titleWidth = 100.0;
@@ -131,23 +104,29 @@ class _RateForTradingState extends State<RateForTrading> {
                               fit: BoxFit.cover,
                             ),
                           ),
-                          const SizedBox(width: 30),
-                          const Text(
-                            'Duy Quang',
-                            style: TextStyle(
-                              fontSize: 21,
+                          const SizedBox(width: 25),
+                          Container(
+                            width: width * 0.5,
+                            child: DefaultTextStyle(
+                              style: const TextStyle(
+                                fontSize: 21,
+                                color: kTextColor,
+                              ),
+                              child: GetUserName(
+                                documentId: otherSideUserID,
+                                isStream: false,
+                              ),
                             ),
                           ),
                         ],
                       ),
-                      const SizedBox(
-                        height: 10,
-                      ),
+                      const SizedBox(height: 10),
                     ],
                   ),
                 ),
                 const SizedBox(height: 20),
                 Container(
+                  alignment: Alignment.topLeft,
                   decoration: const BoxDecoration(
                     //color: Colors.white,
                     border: Border(
@@ -173,7 +152,119 @@ class _RateForTradingState extends State<RateForTrading> {
                       const SizedBox(
                         height: 15,
                       ),
-                      ..._buildProductsList(products),
+                      FutureBuilder<DocumentSnapshot>(
+                          future: referenceDatabase
+                              .collection('tradings')
+                              .doc(tradingID)
+                              .get(),
+                          builder: (context, snapshot) {
+                            if (snapshot.hasError) {
+                              return Text('Something went wrong',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.normal,
+                                    color: Colors.red.withOpacity(0.6),
+                                  ));
+                            }
+
+                            if (snapshot.hasData && !snapshot.data!.exists) {
+                              return Text('Document does not exist',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.normal,
+                                    color: Colors.red.withOpacity(0.6),
+                                  ));
+                            }
+                            if (snapshot.connectionState ==
+                                ConnectionState.done) {
+                              final _trading =
+                                  snapshot.data!.data() as Map<String, dynamic>;
+                              final amIOwner = _trading['owner'] == userID;
+                              final posts = !amIOwner
+                                  ? _trading['ownerPosts'] as List
+                                  : _trading['offerUserPosts'] as List;
+                              return Column(
+                                children: [
+                                  for (var post in posts)
+                                    FutureBuilder<DocumentSnapshot>(
+                                      future: referenceDatabase
+                                          .collection('posts')
+                                          .doc(post.toString())
+                                          .get(),
+                                      builder: (context, snapshot) {
+                                        if (snapshot.hasError) {
+                                          return Text('Something went wrong',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.normal,
+                                                color:
+                                                    Colors.red.withOpacity(0.6),
+                                              ));
+                                        }
+
+                                        if (snapshot.hasData &&
+                                            !snapshot.data!.exists) {
+                                          return Text('Document does not exist',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.normal,
+                                                color:
+                                                    Colors.red.withOpacity(0.6),
+                                              ));
+                                        }
+                                        if (snapshot.connectionState ==
+                                            ConnectionState.done) {
+                                          final post = snapshot.data!.data()
+                                              as Map<String, dynamic>;
+
+                                          return Padding(
+                                            padding: const EdgeInsets.all(8),
+                                            child: Row(
+                                              children: [
+                                                Container(
+                                                  width: 70,
+                                                  height: 70,
+                                                  child: ClipRRect(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            10),
+                                                    child: Image.asset(
+                                                      products[0].images[0],
+                                                      fit: BoxFit.cover,
+                                                    ),
+                                                  ),
+                                                ),
+                                                const SizedBox(width: 30),
+                                                Expanded(
+                                                  child: Text(
+                                                    post['name'].toString(),
+                                                    overflow:
+                                                        TextOverflow.ellipsis,
+                                                    maxLines: 2,
+                                                    style: const TextStyle(
+                                                      fontSize: 16,
+                                                    ),
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          );
+                                        }
+                                        return const Center(
+                                          child: CircularProgressIndicator(
+                                            valueColor:
+                                                AlwaysStoppedAnimation<Color>(
+                                                    Colors.blue),
+                                          ),
+                                        );
+                                      },
+                                    )
+                                ],
+                              );
+                            }
+                            return const Center(
+                              child: CircularProgressIndicator(
+                                valueColor:
+                                    AlwaysStoppedAnimation<Color>(Colors.blue),
+                              ),
+                            );
+                          }),
                       const SizedBox(
                         height: 10,
                       ),
@@ -210,49 +301,39 @@ class _RateForTradingState extends State<RateForTrading> {
                         //   setState(() {
                         //     _rating = rating;
                         //   });
-                        //   // print('rating $_rating');
+                        // print('rating $_rating');
                         // },
                       ),
                     ],
                   ),
                 ),
                 const SizedBox(height: 20),
-                Container(
-                  decoration: const BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(
-                        color: kTextColor,
-                        width: 0.2,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      width: titleWidth,
+                      child: const Text(
+                        'Nhận xét',
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w600,
+                        ),
                       ),
                     ),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.only(bottom: 20),
-                        width: titleWidth,
-                        child: const Text(
-                          'Nhận xét',
-                          style: TextStyle(
-                            fontSize: 17,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    const TextField(
+                      maxLength: 200,
+                      decoration: InputDecoration(
+                        labelText: 'Nhận xét giao dịch',
+                        hintText: 'Nhập lời nhận xét...',
                       ),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      const TextField(
-                        maxLength: 200,
-                        decoration: InputDecoration(
-                          labelText: 'Nhận xét giao dịch',
-                          hintText: 'Nhập lời nhận xét...',
-                        ),
-                        maxLines: 4,
-                      ),
-                    ],
-                  ),
+                      maxLines: 4,
+                    ),
+                  ],
                 )
               ],
             ),
@@ -260,5 +341,13 @@ class _RateForTradingState extends State<RateForTrading> {
         ),
       ),
     );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty('userID', userID));
+    properties.add(DiagnosticsProperty<DocumentReference<Map<String, dynamic>>>(
+        'referenceDatabase', referenceDatabase));
   }
 }
