95d7b3ec42918dce51abc60330198de2ac74673d
NGUYỄN THÀNH VĨNH PHÚC <1712675@student.hcmus.edu.vn>
2021-06-03 13:14:53 +0700

    Replace for old app_config folder

---
+++ lib/constants/app_assets.dart
@@ -0,0 +1,30 @@
+class AppAssets {
+  AppAssets._();
+
+  // static const String authFolder = 'assets/icons/auth';
+  // static const String logoFolder = 'assets/images/logos';
+  // static const String chatScreenAvaFolder = 'assets/images/chat_screen_ava';
+  // static const String productImagesTempFolder =
+  //     'assets/images/products_images_temp';
+  // static const String onBoardingScreenFolder =
+  //     'assets/images/on_boarding_screen';
+
+  // onboarding screen assets
+  static const String firstWalkThroughImage =
+      'assets/images/on_boarding_screen/onboarding_1.png';
+  static const String secondWalkThroughImage =
+      'assets/images/on_boarding_screen/onboarding_2.png';
+  static const String thirdWalkThroughImage =
+      'assets/images/on_boarding_screen/onboarding_3.png';
+
+  // otp screen assets
+  static const String blackCircleIcon = 'assets/icons/auth/black_circle.svg';
+
+  // secondary splash screen assets
+  static const String noInternetConnectionPicture =
+      'assets/images/secondary_splash_screen/no_internet_connection.svg';
+
+  static const String googleIcon = 'assets/icons/auth/google_icon.svg';
+  static const String appleIcon = 'assets/icons/auth/apple_icon.svg';
+  static const String facebookIcon = 'assets/icons/auth/facebook_icon.svg';
+}
