7196be896ce7db4658967739f105a8a6af902189
Duy Quang <61661668+duyquang19981@users.noreply.github.com>
2021-06-10 05:30:14 +0700

    add loading status into change password screen

--- lib/ui/account_features/user_info/change_password_screen.dart
+++ lib/ui/account_features/user_info/change_password_screen.dart
@@ -28,6 +28,61 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
     node.unfocus();
   }
 
+  bool _isLoaded = true;
+
+  Future<void> _savePassword() async {
+    _formKey.currentState!.save();
+    if (_formKey.currentState!.validate()) {
+      if (_confirmPasswordController.text != _newPasswordController.text) {
+        await showMyNotificationDialog(
+            context: context,
+            title: 'Thông báo',
+            content: 'Xác nhận mật khẩu không chính xác.',
+            handleFunction: () {
+              Navigator.of(context).pop();
+            });
+      } else {
+        setState(() {
+          _isLoaded = false;
+        });
+
+        final user = FirebaseAuth.instance.currentUser;
+        if (user != null && !user.emailVerified) {
+          try {
+            final username = user.email.toString();
+            final credential = EmailAuthProvider.credential(
+                email: username, password: _oldPasswordController.text);
+            await FirebaseAuth.instance.currentUser!
+                .reauthenticateWithCredential(credential);
+            await user.updatePassword(_newPasswordController.text);
+            await showMyNotificationDialog(
+                context: context,
+                title: 'Thông báo',
+                content: 'Thay đổi mật khẩu thành công.',
+                handleFunction: () {
+                  Navigator.of(context).pop();
+                  Navigator.of(context).pop();
+                });
+          } on FirebaseAuthException catch (_) {
+            await showMyNotificationDialog(
+                context: context,
+                title: 'Thông báo',
+                content:
+                    'Mật khẩu cũ không đúng hoặc có lỗi xảy ra. Vui lòng thử lại sau.',
+                handleFunction: () {
+                  Navigator.of(context).pop();
+                });
+          }
+          setState(() {
+            _isLoaded = true;
+          });
+        } else {
+          //user must login to do this action
+        }
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     node = FocusScope.of(context);
@@ -36,77 +91,79 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
       appBar: AppBar(
         title: const Text('Đổi mật khẩu'),
       ),
-      body: GestureDetector(
-        onTap: () {
-          FocusScope.of(context).requestFocus(FocusNode());
-        },
-        child: ListView(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 15,
-            vertical: 30,
-          ),
-          children: [
-            FormBuilder(
-              key: _formKey,
-              onChanged: () {},
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              child: Column(
+      body: _isLoaded
+          ? GestureDetector(
+              onTap: () {
+                FocusScope.of(context).requestFocus(FocusNode());
+              },
+              child: ListView(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 30,
+                ),
                 children: [
-                  FormBuilderTextField(
-                    name: 'oldpassword',
-                    decoration: const InputDecoration(
-                      labelText: 'Mật khẩu cũ',
-                      floatingLabelBehavior: FloatingLabelBehavior.auto,
+                  FormBuilder(
+                    key: _formKey,
+                    onChanged: () {},
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    child: Column(
+                      children: [
+                        FormBuilderTextField(
+                          name: 'oldpassword',
+                          decoration: const InputDecoration(
+                            labelText: 'Mật khẩu cũ',
+                            floatingLabelBehavior: FloatingLabelBehavior.auto,
+                          ),
+                          validator: FormBuilderValidators.compose([
+                            FormBuilderValidators.required(context),
+                          ]),
+                          obscureText: true,
+                          onEditingComplete: _onEdittingCompleteHandleFunction,
+                          textInputAction: TextInputAction.done,
+                          keyboardType: TextInputType.text,
+                          controller: _oldPasswordController,
+                        ),
+                        const SizedBox(height: 15),
+                        FormBuilderTextField(
+                          name: 'newpassword',
+                          decoration: const InputDecoration(
+                            labelText: 'Mật khẩu mới',
+                            floatingLabelBehavior: FloatingLabelBehavior.auto,
+                          ),
+                          validator: FormBuilderValidators.compose([
+                            FormBuilderValidators.required(context),
+                            FormBuilderValidators.minLength(context, 6),
+                          ]),
+                          obscureText: true,
+                          onEditingComplete: _onEdittingCompleteHandleFunction,
+                          textInputAction: TextInputAction.done,
+                          keyboardType: TextInputType.text,
+                          controller: _newPasswordController,
+                        ),
+                        const SizedBox(height: 15),
+                        FormBuilderTextField(
+                          name: 'confirmpassword',
+                          decoration: const InputDecoration(
+                            labelText: 'Xác nhận mật khẩu mới',
+                            floatingLabelBehavior: FloatingLabelBehavior.auto,
+                          ),
+                          validator: FormBuilderValidators.compose([
+                            FormBuilderValidators.required(context),
+                            FormBuilderValidators.minLength(context, 6),
+                          ]),
+                          obscureText: true,
+                          onEditingComplete: _onEdittingCompleteHandleFunction,
+                          textInputAction: TextInputAction.done,
+                          keyboardType: TextInputType.text,
+                          controller: _confirmPasswordController,
+                        ),
+                      ],
                     ),
-                    validator: FormBuilderValidators.compose([
-                      FormBuilderValidators.required(context),
-                    ]),
-                    obscureText: true,
-                    onEditingComplete: _onEdittingCompleteHandleFunction,
-                    textInputAction: TextInputAction.done,
-                    keyboardType: TextInputType.text,
-                    controller: _oldPasswordController,
-                  ),
-                  const SizedBox(height: 15),
-                  FormBuilderTextField(
-                    name: 'newpassword',
-                    decoration: const InputDecoration(
-                      labelText: 'Mật khẩu mới',
-                      floatingLabelBehavior: FloatingLabelBehavior.auto,
-                    ),
-                    validator: FormBuilderValidators.compose([
-                      FormBuilderValidators.required(context),
-                      FormBuilderValidators.minLength(context, 6),
-                    ]),
-                    obscureText: true,
-                    onEditingComplete: _onEdittingCompleteHandleFunction,
-                    textInputAction: TextInputAction.done,
-                    keyboardType: TextInputType.text,
-                    controller: _newPasswordController,
-                  ),
-                  const SizedBox(height: 15),
-                  FormBuilderTextField(
-                    name: 'confirmpassword',
-                    decoration: const InputDecoration(
-                      labelText: 'Xác nhận mật khẩu mới',
-                      floatingLabelBehavior: FloatingLabelBehavior.auto,
-                    ),
-                    validator: FormBuilderValidators.compose([
-                      FormBuilderValidators.required(context),
-                      FormBuilderValidators.minLength(context, 6),
-                    ]),
-                    obscureText: true,
-                    onEditingComplete: _onEdittingCompleteHandleFunction,
-                    textInputAction: TextInputAction.done,
-                    keyboardType: TextInputType.text,
-                    controller: _confirmPasswordController,
                   ),
                 ],
               ),
-            ),
-          ],
-        ),
-      ),
+            )
+          : const Center(child: CircularProgressIndicator()),
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(8),
         child: Row(
@@ -114,53 +171,7 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
           children: [
             Expanded(
               child: ElevatedButton(
-                onPressed: () async {
-                  _formKey.currentState!.save();
-                  if (_formKey.currentState!.validate()) {
-                    if (_confirmPasswordController.text !=
-                        _newPasswordController.text) {
-                      await showMyNotificationDialog(
-                          context: context,
-                          title: 'Thông báo',
-                          content: 'Xác nhận mật khẩu không chính xác.',
-                          handleFunction: () {
-                            Navigator.of(context).pop();
-                          });
-                    } else {
-                      final user = FirebaseAuth.instance.currentUser;
-
-                      if (user != null && !user.emailVerified) {
-                        try {
-                          final username = user.email.toString();
-                          final credential = EmailAuthProvider.credential(
-                              email: username,
-                              password: _oldPasswordController.text);
-                          await FirebaseAuth.instance.currentUser!
-                              .reauthenticateWithCredential(credential);
-                          await user
-                              .updatePassword(_newPasswordController.text);
-                          await showMyNotificationDialog(
-                              context: context,
-                              title: 'Thông báo',
-                              content: 'Thay đổi mật khẩu thành công.',
-                              handleFunction: () {
-                                Navigator.of(context).pop();
-                                Navigator.of(context).pop();
-                              });
-                        } on FirebaseAuthException catch (_) {
-                          await showMyNotificationDialog(
-                              context: context,
-                              title: 'Thông báo',
-                              content:
-                                  'Mật khẩu cũ không đúng hoặc có lỗi xảy ra. Vui lòng thử lại.',
-                              handleFunction: () {
-                                Navigator.of(context).pop();
-                              });
-                        }
-                      }
-                    }
-                  }
-                },
+                onPressed: _isLoaded ? _savePassword : null,
                 style: ElevatedButton.styleFrom(
                   primary: Theme.of(context).primaryColor,
                 ),
