9a216093ca8353019f568b95e932c80f022744f7
Duy Quang <61661668+duyquang19981@users.noreply.github.com>
2021-06-18 10:55:04 +0700

    add attributes to Users when signing up

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 import '../models/cloud_firestore/user_model/user/user.dart' as user_model;
 import '../services/firestore/firestore_path.dart';
+import '../ui/message_features/const_string/const_str.dart';
 
 enum Status {
   uninitialized,
@@ -142,9 +143,25 @@ class AuthProvider extends ChangeNotifier {
           ..lastSeen = DateTime.now().millisecondsSinceEpoch
           ..presence = true;
 
+        final initialUserData = _newUser.toJson();
+
+        initialUserData['avatarUrl'] = userImageStr;
+        initialUserData['bio'] = '';
+        initialUserData['createAt'] = DateTime.now();
+        initialUserData['legit'] = 0;
+        initialUserData['ratingCount'] = 0;
+        initialUserData['followers'] = <String>[];
+        initialUserData['following'] = <String>[];
+        initialUserData['hiddenPosts'] = <String>[];
+        initialUserData['location'] = <String>[];
+        initialUserData['objectID'] = _newUser.uid;
+        initialUserData['phoneNumber'] = '';
+        initialUserData['posts'] = <String>[];
+        initialUserData['tradingHistory'] = <String>[];
+
         await Future.wait([
           // Set name in firestore database
-          _usersRef.doc(_newUser.uid).set(_newUser.toJson()),
+          _usersRef.doc(_newUser.uid).set(initialUserData),
           // Set display name
           _firebaseAuthUser.updateDisplayName(name),
         ]);
