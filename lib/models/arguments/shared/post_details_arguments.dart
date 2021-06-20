c029db2ea6b07155aa998843c7450a7b49a9e6f5
phucnguyen1712675 <pbadboyv@gmail.com>
2021-06-20 11:44:04 +0700

    Change: update post details arguments

--- lib/models/arguments/shared/post_details_arguments.dart
+++ lib/models/arguments/shared/post_details_arguments.dart
@@ -1,9 +1,21 @@
-import '../../ui/shared_models/product_model.dart';
+import '../../cloud_firestore/post_card_model/post_card/post_card.dart';
+
+import '../../cloud_firestore/post_details_model/post_details/post_details.dart';
 
 class PostDetailsArguments {
   PostDetailsArguments({
-    required this.postCard,
+    required this.postDetails,
+    required this.isCurrentUserFavoritePost,
+    required this.isCurrentUserAFollowerOfPostOwner,
+    required this.similarPostCards,
+    required this.postOwnerOtherPostCards,
+    required this.postCardsCurrentUserMayAlsoLike,
   });
 
-  final Product postCard;
+  final PostDetails postDetails;
+  final bool isCurrentUserFavoritePost;
+  final bool isCurrentUserAFollowerOfPostOwner;
+  final List<PostCard> similarPostCards;
+  final List<PostCard> postOwnerOtherPostCards;
+  final List<PostCard> postCardsCurrentUserMayAlsoLike;
 }
